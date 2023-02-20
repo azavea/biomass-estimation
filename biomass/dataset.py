31ac727c93670c6e1cf57205980f7e386ac814e2
Lewis Fishgold <lewfish@gmail.com>
2023-02-20 13:38:18 -0500

    Add scripts to reformat images to JPG and modify Dataset

--- biomass/dataset.py
+++ biomass/dataset.py
@@ -26,19 +26,19 @@ fs = s3fs.S3FileSystem(anon=True)
 
 
 class BiomassMetadata():
+    # I'm not totally sure this is the right order for S1
+    s1_bands = ['VV Asc', 'VH Asc', 'VV Desc', 'VH Desc']
+    s2_bands = [
+        'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A', 'B11', 'B12', 'CLP']
+    bands = s1_bands + s2_bands
+    months = [
+        'September', 'October', 'November', 'December', 'January', 'February',
+        'March', 'April', 'May', 'June', 'July', 'August']
+    month2ind = {m: i for i, m in enumerate(months)}
+
     def __init__(self, features_path, labels_path):
         self.features_df = pandas.read_csv(features_path)
         self.labels_df = pandas.read_csv(labels_path)
-
-        # I'm not totally sure this is the right order for S1
-        self.s1_bands = ['VV Asc', 'VH Asc', 'VV Desc', 'VH Desc']
-        self.s2_bands = [
-            'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A', 'B11', 'B12', 'CLP']
-        self.bands = self.s1_bands + self.s2_bands
-        self.months = [
-            'September', 'October', 'November', 'December', 'January', 'February',
-            'March', 'April', 'May', 'June', 'July', 'August']
-        self.month2ind = {m: i for i, m in enumerate(self.months)}
 
     def get_image_uri(self, split, chip_id, satellite, month, root_uri=us_s3_uri):
         return join(root_uri, f'{split}_features/{chip_id}_{satellite}_{month:02}.tif')
@@ -52,19 +52,21 @@ class BiomassMetadata():
 
 class BiomassDataset(Dataset):
     def __init__(self, root_dir='data', split='train', use_best_month=False,
-                 month_items=False, transform=None, no_labels=False, chip_ids=None):
+                 month_items=False, transform=None, chip_ids=None, jpg_format=False):
         if split not in ['train', 'test']:
             raise Exception(f'{split} is not a valid split')
 
         self.root_dir = root_dir
+        self.jpg_format = jpg_format
         self.features_metadata_path = join(root_dir, 'features_metadata.csv')
         self.labels_metadata_path = join(root_dir, 'train_agbm_metadata.csv')
         self.image_dir = join(root_dir, f'{split}_features')
         self.label_dir = join(root_dir, f'{split}_agbm')
 
         self.split = split
+        self.use_best_month = use_best_month
+        self.month_items = month_items
         self.transform = transform
-        self.no_labels = no_labels
         self.metadata = BiomassMetadata(
             self.features_metadata_path, self.labels_metadata_path)
 
@@ -162,42 +164,24 @@ class BiomassDataset(Dataset):
         for task in tqdm(download_tasks, desc='Downloading dataset'):
             download_file(task)
 
-    def getitem_nometadata(self, ind):
+    def get_jpg_item(self, ind):
         chip_id, month_inds = self.chip_id_month_pairs[ind]
+        img_dir = join(self.root_dir, f'jpg_{self.split}_features')
 
         month_arrs = []
-
         for month in month_inds:
-            img_path = self.metadata.get_image_uri(
-                self.split, chip_id, 'S1', month, root_uri=self.root_dir)
-            if isfile(img_path):
-                img_arr = torch.from_numpy(skio.imread(img_path)).permute(2, 0, 1)
-            else:
-                img_arr = torch.zeros(4, 256, 256)
-            s1_arr = img_arr
-
-            img_path = self.metadata.get_image_uri(
-                self.split, chip_id, 'S2', month, root_uri=self.root_dir)
-            if isfile(img_path):
-                # S2 is uint16 and the last band is cloud probability
-                # (ranges 0-100, or 255 for noise)
-                img_arr = torch.from_numpy(
-                    skio.imread(img_path).astype(np.float32)).permute(2, 0, 1)
-            else:
-                img_arr = torch.zeros(11, 256, 256)
-            s2_arr = img_arr
-
-            month_arrs.append(torch.cat([s1_arr, s2_arr]))
+            x = torch.from_numpy(skio.imread(join(img_dir, f'{chip_id}_{month:02}.jpg')))
+            x = x.to(torch.float32) / 255
+            x = x.reshape((15, 256, 256))
+            month_arrs.append(x)
 
         x = torch.stack(month_arrs)
-
         y = None
-        if not self.no_labels:
-            if self.split == 'train':
-                label_path = self.metadata.get_label_uri(
-                    self.split, chip_id, root_uri=self.root_dir)
-                # labels are float32 with zero used for missing data and real values
-                y = torch.from_numpy(skio.imread(label_path)).squeeze()
+        if self.split == 'train':
+            label_path = self.metadata.get_label_uri(
+                self.split, chip_id, root_uri=self.root_dir)
+            # labels are float32 with zero used for missing data and real values
+            y = torch.from_numpy(skio.imread(label_path)).squeeze()
 
         chip_metadata = {
             'chip_id': chip_id,
@@ -212,7 +196,41 @@ class BiomassDataset(Dataset):
         else:
             return x, y, chip_metadata
 
+    def get_jpg_year_item(self, ind):
+        chip_id = self.chip_id_month_pairs[ind][0]
+        img_dir = join(self.root_dir, f'jpg_year_{self.split}_features')
+
+        x = torch.from_numpy(skio.imread(join(img_dir, f'{chip_id}.jpg')))
+        x = x.to(torch.float32) / 255
+        x = torch.stack(torch.split(x, 256), dim=0).reshape(12, 15, 256, 256)
+
+        y = None
+        if self.split == 'train':
+            label_path = self.metadata.get_label_uri(
+                self.split, chip_id, root_uri=self.root_dir)
+            # labels are float32 with zero used for missing data and real values
+            y = torch.from_numpy(skio.imread(label_path)).squeeze()
+
+        chip_metadata = {
+            'chip_id': chip_id,
+            'months': torch.arange(0, 12),
+        }
+
+        if self.transform:
+            x, y = self.transform((x, y))
+
+        if y is None:
+            return x, chip_metadata
+        else:
+            return x, y, chip_metadata
+
     def __getitem__(self, ind):
+        if self.jpg_format:
+            if self.month_items:
+                return self.get_jpg_item(ind)
+            else:
+                return self.get_jpg_year_item(ind)
+
         chip_id, month_inds = self.chip_id_month_pairs[ind]
 
         full_months = []
@@ -260,13 +278,16 @@ class BiomassDataset(Dataset):
         else:
             return x, y, chip_metadata
 
-    def plot_sample(self, x, y, chip_metadata, z=None, out_path=None):
+    @staticmethod
+    def plot_sample(x, y, chip_metadata, z=None, out_path=None):
+        if x.dim() == 3:
+            x = x.unsqueeze(0)
         nrows = x.shape[0] + 1
         ncols = 17
-        s1_masks = chip_metadata['s1_masks']
-        s2_masks = chip_metadata['s2_masks']
+        s1_masks = chip_metadata.get('s1_masks')
+        s2_masks = chip_metadata.get('s2_masks')
         months = chip_metadata['months']
-        col_names = self.metadata.bands + ['S1 Mask', 'S2 Mask']
+        col_names = BiomassMetadata.bands + ['S1 Mask', 'S2 Mask']
 
         fig, axs = plt.subplots(
             nrows, ncols, constrained_layout=True, figsize=(1.5 * ncols, 1.5 * nrows),
@@ -288,16 +309,18 @@ class BiomassDataset(Dataset):
                 for col_ind, ax in enumerate(row_axs):
                     # Add S1 and S2 masks to the last columns.
                     if col_ind == 15:
-                        _x = s1_masks[row_ind]
+                        if s1_masks is not None:
+                            _x = s1_masks[row_ind]
                     elif col_ind == 16:
-                        _x = s2_masks[row_ind]
+                        if s2_masks is not None:
+                            _x = s2_masks[row_ind]
                     else:
                         _x = x[row_ind, col_ind, :, :]
 
                     ax.imshow(_x)
 
                     if col_ind == 0:
-                        ax.set_ylabel(self.metadata.months[months[row_ind]])
+                        ax.set_ylabel(BiomassMetadata.months[months[row_ind]])
                     if row_ind == 0:
                         ax.set_title(col_names[col_ind])
 
@@ -317,8 +340,8 @@ class BiomassDataModule(pl.LightningDataModule):
     def __init__(
             self, root_dir='data', train_ratio=0.8, batch_size=8,
             num_workers=8, dataset_limit=None, use_best_month=False,
-            month_items=False, transforms=None, no_labels=False,
-            shuffle_train=True, pin_memory=True) -> None:
+            month_items=False, transforms=None,
+            shuffle_train=True, pin_memory=True, jpg_format=False) -> None:
         super().__init__()
         self.root_dir = root_dir
         self.train_ratio = train_ratio
@@ -328,9 +351,9 @@ class BiomassDataModule(pl.LightningDataModule):
         self.use_best_month = use_best_month
         self.month_items = month_items
         self.transforms = transforms
-        self.no_labels = no_labels
         self.shuffle_train = shuffle_train
         self.pin_memory = pin_memory
+        self.jpg_format = jpg_format
 
     def setup(self, stage: Optional[str] = None) -> None:
         """Initialize the main ``Dataset`` objects.
@@ -344,14 +367,15 @@ class BiomassDataModule(pl.LightningDataModule):
             None if self.transforms is None else build_transform(self.transforms))
         all_train_dataset = BiomassDataset(
             root_dir=self.root_dir, split='train', use_best_month=self.use_best_month,
-            month_items=self.month_items, transform=transform, no_labels=self.no_labels)
+            month_items=self.month_items, transform=transform,
+            jpg_format=self.jpg_format)
         if self.dataset_limit is not None and self.dataset_limit < len(all_train_dataset):
             all_train_dataset = Subset(all_train_dataset, list(range(self.dataset_limit)))
         self.train_dataset, self.val_dataset = random_split(
             all_train_dataset, [self.train_ratio, 1-self.train_ratio])
         self.predict_dataset = BiomassDataset(
             root_dir=self.root_dir, split='test', use_best_month=self.use_best_month,
-            month_items=self.month_items, transform=transform)
+            month_items=self.month_items, transform=transform, jpg_format=self.jpg_format)
         if self.dataset_limit is not None and self.dataset_limit < len(self.predict_dataset):
             self.predict_dataset = Subset(
                 self.predict_dataset, list(range(self.dataset_limit)))
